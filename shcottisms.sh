e0a49c0a9505d03855f70bc04430ac9bfa3c5940
7Ritz <777ritz@proton.me>
2026-07-21 10:47:05 +0100

    Init Repo

---
+++ shcottisms.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scottism=$(($RANDOM % 10))
+
+# List of Scottisms That Could Show
+scottism1="Computer Science is the Answer to Everything"
+
+if "1" is echoed 
+	then echo "$scottism1"
+
+ 
